9cb69136ea92b19b9975c998c487baf3128b6dd4
Hemant Rana <hemantrana000786@gmail.com>
2025-08-06 18:43:41 +0530

    Solve the bug of todo creation Have issue of date formate

--- src/main.ts
+++ src/main.ts
@@ -16,7 +16,7 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true,
     forbidNonWhitelisted: true,
-    transform: true,
+    transform: false,
   }));
 
   // Swagger documentation setup
